adcbb7ab23e4cef710d87240856b235749c8410c
Maria Møll <marim18@uia.no>
2024-02-08 17:17:05 +0100

    refractored task 1 to use things from here in task 3

--- task1.cs
+++ task1.cs
@@ -1,21 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 using functions;
 using System;
 Func nfu = new Func();
 
-void pattern1(){
-    nfu.MoveChain();
-    nfu.Turn();
-    nfu.MoveChain();
-    nfu.Turn();
-    nfu.MoveChain();
-    nfu.trippleturn();
-    nfu.MoveChain();
-
-}
 while (!nfu.AtGoal())
 {
-  pattern1();
+  nfu.pattern1();
 
 }
 Console.WriteLine("you made it");
