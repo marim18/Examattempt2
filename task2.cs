0bb942aeba17b1a3900660fb29ef949203ecf6b8
Maria Møll <marim18@uia.no>
2024-02-08 17:07:10 +0100

    refractored task 2 again

--- task2.cs
+++ task2.cs
@@ -1,9 +1,9 @@
 using functions;
 using System;
 Func nfu = new Func();
-    while (!nfu.AtGoal())
-    {
-       nfu.MoveChain();
-       nfu.Turn();
-    }
-    Console.WriteLine("you made it");
+while (!nfu.AtGoal())
+{
+    nfu.MoveChain();
+    nfu.Turn();
+}
+Console.WriteLine("you made it");
